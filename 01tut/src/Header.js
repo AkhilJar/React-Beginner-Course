a1841769a9d10d1b878a43a8edc8054e36618a04
DoffyInstitutes <akhil.jarabandi13@gmail.com>
2026-01-31 23:17:37 -0600

    css in line + object style

--- 01tut/src/Header.js
+++ 01tut/src/Header.js
@@ -1,8 +1,14 @@
 import React from "react";
 
 const Header = () => {
+  //can write in line style or can create style "objects" that have attributes and then link later
+  const headerStyle = {
+    backgroundColor: "royalblue",
+    color: "#fff",
+  };
+
   return (
-    <header>
+    <header style={headerStyle}>
       <h1>Groceries List</h1>
     </header>
   );
