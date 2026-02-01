81ad41c4849742840e71cd7e8ea3556d28460a9b
DoffyInstitutes <akhil.jarabandi13@gmail.com>
2026-01-31 21:07:34 -0600

    end chapter 2 - learn jsx:

--- 01tut/src/App.js
+++ 01tut/src/App.js
@@ -1,22 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
+//function to create components, arrow function usually
 function App() {
+  //can write HTML and javascript = JS
+  //JSX is template for component layout; renders as text
+  const name = "Dave";
+
+  //convention to start functions with handle
+  const handleNameChange = () => {
+    const names = ["Bob", "Kevin", "Dave"];
+    const int = Math.floor(Math.random() * 3);
+    return names[int];
+  };
+
   return (
+    //class reserved so use className for attribute
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Hello {handleNameChange()}</p>
+        <p>Hello {name}!</p>
+        {/* Curly script means this is a javascript expression 
+            - useful because it parses as text 
+            - booleans and objects don't render
+        */}
+        {/* <p>{name}</p> */}
       </header>
     </div>
   );
