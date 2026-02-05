6c9ecf8ac26ad7d396386fb7ee2c02618987c510
DoffyInstitutes <akhil.jarabandi13@gmail.com>
2026-02-05 01:17:36 -0600

    use state and useState issue (batch rendering

--- 01tut/src/Header.js
+++ 01tut/src/Header.js
@@ -1,14 +1,8 @@
 import React from "react";
 
 const Header = () => {
-  //can write in line style or can create style "objects" that have attributes and then link later
-  const headerStyle = {
-    backgroundColor: "royalblue",
-    color: "#fff",
-  };
-
   return (
-    <header style={headerStyle}>
+    <header>
       <h1>Groceries List</h1>
     </header>
   );
